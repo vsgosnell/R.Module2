3b5d901feaa4941fbc30ab0932648f842d44dec4
Veronica Gosnell <vsgosnell@usf.edu>
2025-01-24 20:12:14 -0500

    from the practicing functions pdf

--- functions.with.mean.R
+++ functions.with.mean.R
@@ -13,9 +13,9 @@ Mean <- function(X) {M = sum(X)/length(X); M}
 Mean(quarters)
 
 Mean = function(X) {
-  S = sum(X)
-  L = length(X)
-  M = S/L
+  S <<- sum(X)
+  L <<- length(X)
+  M <<- S/L
   s = c("Sum is:", S) # creates the character string variable 's'
   l = c("Length is:", L) # in which S is converted to a character string; ditto for l and m
   m = c("Mean is:", M)
@@ -23,3 +23,5 @@ Mean = function(X) {
   print(l, quote = FALSE) # normally around a chararcter string
   print(m, quote = FALSE)
 }
+
+
